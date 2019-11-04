88987107ed3fd853a6cf2dde3146b8dc8ee8cca2
Khuong Tran <tranhoangkhuongvn@gmail.com>
2019-11-04 17:22:31 +1100

    Add two solutions for phone letter conversion

--- phone_letter_combination.py
+++ phone_letter_combination.py
@@ -13,34 +13,27 @@ def solution():
 
 	ins = '23'
 	ans = []
-	result = []
 	current_index = 0
-	def conversion(ins, ans, current_index, result):
+	def conversion(ins, ans, current_index):
 		n = len(ins)
 		if len(ans) == n:
 			result.append(ans)	
-			print(ans)
-			print(result)
-			return result 
-
+			print(result)	
+		else:
 		
-		num = ins[current_index]
-		possible_char = list(number_letter_map[num])
+			num = ins[current_index]
+			possible_char = list(number_letter_map[num])
+			
+			for j in range(len(possible_char)):
+				ans.append(possible_char[j])
+				conversion(ins, ans, current_index+1)
+				ans.pop()
+			
 		
-		for j in range(len(possible_char)):
-			ans.append(possible_char[j])
-			temp = conversion(ins, ans, current_index+1, result)
-			if temp and len(temp) == 2:
-				result.append(temp)
-			ans.pop()
 
-		return None 
-
-
-	result = conversion(ins, ans, current_index, result)
+	result = []
+	conversion(ins, ans, current_index)
 	
-	for res in result:
-		print(res)
 
 	return result
 
